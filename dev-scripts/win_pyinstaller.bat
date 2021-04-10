df11726dbc87f577eeada550bd64d9a82adc5461
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-04-10 13:14:44 +0200

    win installer

--- dev-scripts/win_pyinstaller.bat
+++ dev-scripts/win_pyinstaller.bat
@@ -2,4 +2,4 @@ python -m virtualenv venv
 
 .\venv\Scripts\pip.exe install -r requirements.txt
 .\venv\Scripts\pip.exe install pyinstaller
-.\venv\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico email_feedback_spss_exam.py
+.\venv\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico sharestats_item_editor.py
