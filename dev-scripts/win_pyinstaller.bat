68f7fba5b9332f0ef350bb123541078df71854d0
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-04-10 16:58:13 +0200

    update: win installer

--- dev-scripts/win_pyinstaller.bat
+++ dev-scripts/win_pyinstaller.bat
@@ -1,5 +1,5 @@
 python -m virtualenv venv
 
 .\venv\Scripts\pip.exe install -r requirements.txt
-.\venv\Scripts\pip.exe install pyinstaller
+.\venv\Scripts\pip.exe install --index-url https://test.pypi.org/simple/ sharestats-item-editor
 .\venv\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico sharestats_item_editor.py
