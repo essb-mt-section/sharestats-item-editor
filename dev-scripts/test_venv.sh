189765a596826dc3b4db31947e56928e564c2c5a
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-05-07 17:05:46 +0200

    test venv

---
+++ dev-scripts/test_venv.sh
@@ -0,0 +1,6 @@
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install appdirs pysimplegui
+python -m pip install --index-url https://test.pypi.org/simple/ -U sharestats_item_editor
+python -m sharestats_item_editor
+
