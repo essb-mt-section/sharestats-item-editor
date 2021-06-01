875ee6f74d9dee09b699ae684ef7aea82dd3c881
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-06-01 19:02:22 +0200

    v0.2

--- sharestats_item_editor/item_editor/__init__.py
+++ sharestats_item_editor/item_editor/__init__.py
@@ -3,7 +3,7 @@
 (c) Oliver Lindemann
 """
 
-__version__ = '0.1.23'
+__version__ = '0.2'
 __author__ = 'Oliver Lindemann'
 APPNAME = "RExam Item Editor"
 WEBSITE = "https://github.com/essb-mt-section/sharestats-item-editor"
