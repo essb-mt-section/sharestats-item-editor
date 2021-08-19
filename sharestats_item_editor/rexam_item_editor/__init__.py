4bb180baf8960d5da7477c1be2e2784d9b1bfe72
0liver Lindemann <lindemann09@googlemail.com>
2021-08-19 16:01:58 +0200

    v0.2.3

--- sharestats_item_editor/rexam_item_editor/__init__.py
+++ sharestats_item_editor/rexam_item_editor/__init__.py
@@ -3,7 +3,7 @@
 (c) Oliver Lindemann
 """
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'Oliver Lindemann'
 APPNAME = "RExam Item Editor"
 WEBSITE = "https://github.com/lindemann09/rexam-item-editor"
