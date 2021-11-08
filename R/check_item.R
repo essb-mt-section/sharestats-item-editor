08c16a3518528f61e3090980d29b1e522a4a45da
Oliver Lindemann <lindemann09@users.noreply.github.com>
2021-11-08 13:48:05 +0100

    Update check_item.R

--- R/check_item.R
+++ R/check_item.R
@@ -2,6 +2,9 @@ check_item <- function(...) {
   if (!("exams" %in% installed.packages()[ , "Package"])){
     install.packages("exams")
   }
+  if (!("rstudioapi" %in% installed.packages()[ , "Package"])){
+    install.packages("rstudioapi")
+  }
   file = rstudioapi::getSourceEditorContext()$path
   exams::exams2html(file)
 }
